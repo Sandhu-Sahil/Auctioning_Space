901d94baa87ab26e81e6e10c9cd31b108e3399fe
Sandhu-Sahil <sandhu.sahil2002@gmail.com>
2024-04-29 22:37:24 +0530

    mobile view

--- auctions/views.py
+++ auctions/views.py
@@ -13,12 +13,12 @@ class AuctionForm(forms.ModelForm):
     class Meta:
         model = Auction
         fields = ('title', 'description', 'starting_bid', 'category', 'person', 'image')
-        #widgets = {#'category' : forms.Select(choices=Category.objects.all(), attrs={'class' : 'form-control'}),
-                   #'person' : forms.Select(choices=Person.objects.all(), attrs={'class' : 'form-control'}),
-                   #'title': forms.TextInput(attrs={'class': 'form-control'}),
-                   #'description': forms.TextInput(attrs={'class': 'form-control'}),
-                   #'starting_bid': forms.NumberInput(attrs={'class': 'form-control'})
-                   #} 
+        widgets = {'category' : forms.Select(choices=Category.objects.all(), attrs={'class' : 'form-control'}),
+                   'person' : forms.Select(choices=Person.objects.all(), attrs={'class' : 'form-control'}),
+                   'title': forms.TextInput(attrs={'class': 'form-control'}),
+                   'description': forms.TextInput(attrs={'class': 'form-control'}),
+                   'starting_bid': forms.NumberInput(attrs={'class': 'form-control'})
+                   } 
 
 
 def index(request):
